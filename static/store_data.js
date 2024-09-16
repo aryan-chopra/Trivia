4de7d205b0fdb42bc43dacecf13817db62795f87
aryan-chopra <aryan.chopra1211@gmail.com>
2024-09-16 10:35:38 +0530

    Added to local storage

---
+++ static/store_data.js
@@ -0,0 +1,11 @@
+function doSomething() {
+    const movieName = document.getElementById("movie-name").value
+    const movieBannerURL = document.getElementById("movie-banner-url").value
+    console.log(movieName)
+    console.log(movieBannerURL)
+
+    let movieData = localStorage.getItem(movieName)
+    if (!movieData) {
+        console.log("Data does not exist for the movie")
+    }
+}

d2d53e804ad4da15c3ae4de8e91d390ec411cc7f
aryan-chopra <aryan.chopra1211@gmail.com>
2024-09-24 15:49:58 +0530

    options now appear shuffled

--- static/trivia.js
+++ static/trivia.js
@@ -15,7 +15,7 @@ function createQuestionBox() {
 
     statement = document.createElement("p")
     statement.setAttribute("id", "trivia-question-statement")
-    questionBox.appendChild(statement)
+    questionBox.insertAdjacentElement('afterbegin', statement)
 
     optionIndex = 0
     while (optionIndex < 4) {
@@ -31,35 +31,80 @@ function createQuestionBox() {
         optionButton.setAttribute("name", "option")
 
         label.appendChild(optionButton)
-        questionBox.appendChild(label)
+        statement.insertAdjacentElement('afterend', label)
 
         optionIndex++
     }
 
-    submitButton = document.createElement("button")
-    submitButton.setAttribute("id", "trivia-submit-button")
-    submitButton.innerHTML = "Submit"
-    questionBox.appendChild(submitButton)
+    // submitButton = document.createElement("button")
+    // submitButton.setAttribute("id", "trivia-submit-button")
+    // submitButton.innerHTML = "Submit"
+    // questionBox.appendChild(submitButton)
 }
 
 function displayQuestion(question) {
     statement = document.getElementById("trivia-question-statement")
     statement.innerHTML = question.statement
 
-    optionIndex = 0
-    for (let option of question.options) {
-        optionButton = document.getElementById("trivia-option-" + optionIndex)
+    let questionState = localStorage.getItem(question.statement)
+
+    if (questionState) {
+        return loadExistingQuestion(JSON.parse(questionState))
+    }
+
+    else {
+        return createQuestionState(question)
+    }
+}
+
+function loadExistingQuestion(questionState) {
+    loadOptions(questionState.options)
+
+    console.log("Getting: trivia-option-" + questionState.selectedOptionIndex)
+
+    let checkedOption = document.getElementById("trivia-option-" + questionState.selectedOptionIndex)
+    console.log(checkedOption)
+    checkedOption.checked = true
+
+    return questionState
+}
+
+function loadOptions(options) {
+    for (let option in options) {
+        optionButton = document.getElementById("trivia-option-" + option)
         optionButton.checked = false
 
-        label = document.getElementById("trivia-option-label-" + optionIndex)
-        label.innerHTML = option;
+        label = document.getElementById("trivia-option-label-" + option)
+        label.innerHTML = options[option]
 
         label.appendChild(optionButton)
-        console.log(optionButton)
-        console.log("hi")
-
-        optionIndex++
     }
+}
+
+function createQuestionState(question) {
+    let questionState = {
+        options: new Array(),
+        correctOptionIndex: -1,
+        selectedOptionIndex: 0
+    }
+
+    let optionToSet = 0
+    while (question.options.length > 0) {
+        let optionIndex = Math.floor(Math.random() * question.options.length)
+        let option = question.options.splice(optionIndex, 1)[0]
+
+        questionState.options.push(option)
+
+        if (questionState.correctOptionIndex == -1 && optionIndex == 0) {
+            questionState.correctOptionIndex = optionToSet
+        }
+
+        optionToSet++
+    }
+
+    loadOptions(questionState.options)
+
+    return questionState
 }
 
 function buttonClick() {
@@ -72,6 +117,16 @@ function buttonClick() {
     })
 }
 
+function getSelectedButton() {
+    let buttons = document.getElementsByName("option")
+
+    for (let button of buttons) {
+        if (button.checked) {
+            return parseInt(button.id[button.id.length - 1])
+        }
+    }
+}
+
 async function extractAndLoadQuestions(trivia) {
     let currentQuestion = 0
 
@@ -80,16 +135,20 @@ async function extractAndLoadQuestions(trivia) {
     }
 
     while (currentQuestion < 5) {
-        question = trivia.questions[currentQuestion]
-        displayQuestion(question)
+        let question = trivia.questions[currentQuestion]
+        let questionState = displayQuestion(question)
 
         await buttonClick()
-        console.log("clicked")
 
-        if (document.getElementById("trivia-option-0").checked) {
+        let selectedButtonIndex = getSelectedButton()
+        questionState.selectedOptionIndex = selectedButtonIndex
+
+        if (selectedButtonIndex == questionState.correctOptionIndex) {
             result.correctQuestions++;
         }
 
+
+        localStorage.setItem(question.statement, JSON.stringify(questionState))
         currentQuestion++;
     }
 
